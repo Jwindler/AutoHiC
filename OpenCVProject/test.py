26dc20c2e0e79514758f81ad9f894222c31a7286
Swindler <1033199817@qq.com>
2022-03-23 22:10:42 +0800

    update 3.23 Learn

--- OpenCVProject/test.py
+++ OpenCVProject/test.py
@@ -13,10 +13,27 @@
 import cv2 as cv
 from matplotlib import pyplot as plt
 import numpy as np
+from scipy import ndimage
 
-img_path = "./images/HiC2.png"
-img = cv.imread(img_path)
+img = cv.imread('./images/HiC2.png', 0)
 
-cv.imshow('a', img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+kernel_3x3 = np.array([[-1, -1, -1],
+                      [-1, 8, -1],
+                      [-1, -1, -1]])
+
+kernel_5x5 = np.array([[-1, -1, -1, -1, -1],
+                      [-1, 1, 2, 1, -1],
+                      [-1, 2, 4, 2, -1],
+                      [-1, 1, 2, 1, -1],
+                      [-1, -1, -1, -1, -1]])
+
+k3 = ndimage.convolve(img, kernel_3x3)
+k5 = ndimage.convolve(img, kernel_5x5)
+
+blurred = cv.GaussianBlur(img, (11, 11), 0)
+g_hpf = img - blurred
+
+plt.subplot(131); plt.imshow(k3); plt.title('k3')
+plt.subplot(132); plt.imshow(k5); plt.title('k4')
+plt.subplot(133); plt.imshow(g_hpf); plt.title('g_hpf')
+plt.show()
