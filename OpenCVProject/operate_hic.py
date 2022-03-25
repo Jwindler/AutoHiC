d88959208015172586978a0b75cdd64c13385bbb
Swindler <1033199817@qq.com>
2022-03-25 20:36:29 +0800

    update

---
+++ OpenCVProject/operate_hic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python 
+# encoding: utf-8 
+
+"""
+@author: Swindler
+@contact: 1033199817@qq.com
+@file: operate_hic.py
+@time: 2022/3/24 下午8:11
+@function: 
+"""
+
+import hicstraw
+
