beaf6ceb4800a6ba2e813d13545c103428214306
Swindler <jzj@1033199817@qq.com>
2022-11-08 19:33:16 +0800

    fix error class errors and add recitfy all category error flow code

--- tests/error.py
+++ tests/error.py
@@ -9,10 +9,6 @@
 @function: 
 """
 
-import json
-import numpy as np
-import pickle
-
 
 class ERRORS:
     def __init__(self, classes, info_file, img_size=(1110, 1100)):
@@ -27,8 +23,8 @@ class ERRORS:
             self.errors[class_] = []
 
     # 生成错误结构
-    def create_structure(self, img_info, detection_result):
-
+    def create_structure(self, img_info, detection_result, epoch_flag=None):
+        # TODO: epoch_flag used to distinguish whether the epoch is latest or not
         for category, classes in zip(detection_result[0], self.classes):
             for index, error in enumerate(category):
                 error = error.tolist()
@@ -69,9 +65,34 @@ class ERRORS:
         b_s = y * h_ration + img_chr_b_s
         b_e = h * h_ration + img_chr_b_s
 
-        hic_loci = list(map(lambda x: int(x), [a_s, a_e, b_s, b_e]))
+        hic_loci = list(map(lambda temp: int(temp), [a_s, a_e, b_s, b_e]))
 
         return hic_loci
+
+    @staticmethod
+    def cal_iou(box1, box2):
+        x1min, y1min, x1max, y1max = box1[0], box1[1], box1[2], box1[3]
+        x2min, y2min, x2max, y2max = box2[0], box2[1], box2[2], box2[3]
+
+        # calculate box area
+        s1 = (y1max - y1min + 1.) * (x1max - x1min + 1.)
+        s2 = (y2max - y2min + 1.) * (x2max - x2min + 1.)
+
+        # calculate overlap area
+        x_min = max(x1min, x2min)
+        y_min = max(y1min, y2min)
+        x_max = min(x1max, x2max)
+        y_max = min(y1max, y2max)
+
+        inter_h = max(y_max - y_min + 1, 0)
+        inter_w = max(x_max - x_min + 1, 0)
+
+        intersection = inter_h * inter_w
+        union = s1 + s2 - intersection
+
+        # calculate iou
+        iou = intersection / union
+        return iou
 
     # 根据score，过滤错误
     def filter_all_errors(self, score: float = 0.9, filter_cls=None):
@@ -83,33 +104,91 @@ class ERRORS:
 
         return filtered_errors
 
-    # TODO: 去除同类错误相交错误
-    def de_same_overlap(self):
-        pass
+    @staticmethod
+    def transform_bbox(detection_bbox):
+        """
+        transform bbox to [x1, y1, x2, y2]
+        :param detection_bbox: [x, y, w, h]
+        :return:
+        """
+        x1 = detection_bbox[0]
+        y1 = detection_bbox[1]
+        x2 = detection_bbox[0] + detection_bbox[2]
+        y2 = detection_bbox[1] + detection_bbox[3]
+        return [x1, y1, x2, y2]
 
-    # TODO: 去除异类相交错误
-    def de_diff_overlap(self):
-        pass
+    def de_diff_overlap(self, errors_dict: dict, iou_score: float = 0.9):
+        remove_list = list()  # save the key of the errors_dict which has been removed
+        ans = []  # store de_overlap errors
+        all_errors = []
+        for class_ in errors_dict:  # loop classes
+            all_errors += errors_dict[class_]
+            # loop errors
+        sorted_errors_dict = sorted(all_errors, key=lambda itme: itme["hic_loci"][0], reverse=False)
+
+        for error in sorted_errors_dict:
+
+            # whether there is overlap
+            if ans and error["hic_loci"][0] <= ans[-1]["hic_loci"][1]:
+
+                # calculate overlap ratio
+                bbox1 = self.transform_bbox(error["bbox"])
+                bbox2 = self.transform_bbox(ans[-1]["bbox"])
+                counted_score = self.cal_iou(bbox1, bbox2)
+                if counted_score > iou_score:
+                    # judge which one's resolution is higher
+                    if error["resolution"] == ans[-1]["resolution"]:
+                        ans.append(max(error, ans[-1], key=lambda item: item["score"]))
+                        remove_list.append((error, ans[-1]))
+                    else:
+                        # select the error with the highest resolution
+                        ans.append(max(error, ans[-1], key=lambda item: item["resolution"]))
+                        remove_list.append((error, ans[-1]))
+                else:  # not same error but have overlap
+                    # FIXME: not same error but have overlap
+                    # Select the middle position of the two errors
+                    last_copy = ans[-1]
+                    error_copy = error
+                    last_copy["hic_loci"][1] = error_copy["hic_loci"][0] + int(
+                        (last_copy["hic_loci"][1] - error_copy["hic_loci"][0]) / 2) - 1
+
+                    error_copy["hic_loci"][0] = last_copy["hic_loci"][1] + 1
+
+                    last_copy["hic_loci"][3] = error_copy["hic_loci"][2] + int(
+                        (last_copy["hic_loci"][3] - error_copy["hic_loci"][2]) / 2) - 1
+
+                    error_copy["hic_loci"][2] = last_copy["hic_loci"][3] + 1
+
+                    ans.append(last_copy)
+                    ans.append(error_copy)
+                    remove_list.append((error, ans[-1]))
+                    # raise NotImplementedError("Not same error but have overlap, wait to solve")
+            else:  # nought overlap
+                ans.append(error)
+
+        print("Filter all error category Done")
+        return ans
 
 
 def main():
-    file = "/home/jzj/Jupyter-Docker/buffer/Axis.pkl"
-
-    with open(file, 'rb') as f:  # 打开文件
-        result = pickle.load(f)  # 将二进制文件对象转换成 Python 对象
-
-    img_info = {
-        "/root/7.png/png/Aa_inv/2500000/4e93744fcaad4c75936af4f2345db586.jpg": {"genome_id": "Aa_inv",
-                                                                                "chr_A": "assembly",
-                                                                                "chr_A_start": 230700000,
-                                                                                "chr_A_end": 326650000,
-                                                                                "chr_B": "assembly",
-                                                                                "chr_B_start": 248650000,
-                                                                                "chr_B_end": 302750000}}
-    classes = ("translocation", "inversion", "debris", "chromosome")
-    temp = ERRORS(classes, img_info)
-    errors = temp.create_structure(img_info, result)
-    print(errors)
+    # file = "/home/jzj/Jupyter-Docker/buffer/Axis.pkl"
+    #
+    # with open(file, 'rb') as f:  # 打开文件
+    #     result = pickle.load(f)  # 将二进制文件对象转换成 Python 对象
+    #
+    # img_info = {
+    #     "/root/7.png/png/Aa_inv/2500000/4e93744fcaad4c75936af4f2345db586.jpg": {"genome_id": "Aa_inv",
+    #                                                                             "chr_A": "assembly",
+    #                                                                             "chr_A_start": 230700000,
+    #                                                                             "chr_A_end": 326650000,
+    #                                                                             "chr_B": "assembly",
+    #                                                                             "chr_B_start": 248650000,
+    #                                                                             "chr_B_end": 302750000}}
+    # classes = ("translocation", "inversion", "debris", "chromosome")
+    # temp = ERRORS(classes, img_info)
+    # errors = temp.create_structure(img_info, result)
+    # print(errors)
+    pass
 
 
 if __name__ == "__main__":
--- tests/test.py
+++ tests/test.py
@@ -9,106 +9,13 @@
 @function: 
 """
 
-import pickle
 
-
-def cal_iou(box1, box2):
-    x1min, y1min, x1max, y1max = box1[0], box1[1], box1[2], box1[3]
-    x2min, y2min, x2max, y2max = box2[0], box2[1], box2[2], box2[3]
-
-    # calculate box area
-    s1 = (y1max - y1min + 1.) * (x1max - x1min + 1.)
-    s2 = (y2max - y2min + 1.) * (x2max - x2min + 1.)
-
-    # calculate overlap area
-    x_min = max(x1min, x2min)
-    y_min = max(y1min, y2min)
-    x_max = min(x1max, x2max)
-    y_max = min(y1max, y2max)
-
-    inter_h = max(y_max - y_min + 1, 0)
-    inter_w = max(x_max - x_min + 1, 0)
-
-    intersection = inter_h * inter_w
-    union = s1 + s2 - intersection
-
-    # calculate iou
-    iou = intersection / union
-    return iou
-
-
-def transform_bbox(detection_bbox):
-    """
-    transform bbox to [x1, y1, x2, y2]
-    :param detection_bbox: [x, y, w, h]
-    :return:
-    """
-    x1 = detection_bbox[0]
-    y1 = detection_bbox[1]
-    x2 = detection_bbox[0] + detection_bbox[2]
-    y2 = detection_bbox[1] + detection_bbox[3]
-    return [x1, y1, x2, y2]
-
-
-def de_diff_overlap(errors_dict: dict, iou_score: float = 0.9):
-    remove_list = list()  # save the key of the errors_dict which has been removed
-    ans = []  # store de_overlap errors
-    all_errors = []
-    for class_ in errors_dict:  # loop classes
-        all_errors += errors_dict[class_]
-        # loop errors
-    sorted_errors_dict = sorted(all_errors, key=lambda itme: itme["hic_loci"][0], reverse=False)
-
-    for error in sorted_errors_dict:
-
-        # whether there is overlap
-        if ans and error["hic_loci"][0] <= ans[-1]["hic_loci"][1]:
-
-            # calculate overlap ratio
-            bbox1 = transform_bbox(error["bbox"])
-            bbox2 = transform_bbox(ans[-1]["bbox"])
-            counted_score = cal_iou(bbox1, bbox2)
-            if counted_score > iou_score:
-                # judge which one's resolution is higher
-                if error["resolution"] == ans[-1]["resolution"]:
-                    ans.append(max(error, ans[-1], key=lambda item: item["score"]))
-                    remove_list.append((error, ans[-1]))
-                else:
-                    # select the error with the highest resolution
-                    ans.append(max(error, ans[-1], key=lambda item: item["resolution"]))
-                    remove_list.append((error, ans[-1]))
-            else:  # not same error but have overlap
-                # FIXME: not same error but have overlap
-                # Select the middle position of the two errors
-                last_copy = ans[-1]
-                error_copy = error
-                last_copy["hic_loci"][1] = error_copy["hic_loci"][0] + int(
-                    (last_copy["hic_loci"][1] - error_copy["hic_loci"][0]) / 2) - 1
-
-                error_copy["hic_loci"][0] = last_copy["hic_loci"][1] + 1
-
-                last_copy["hic_loci"][3] = error_copy["hic_loci"][2] + int(
-                    (last_copy["hic_loci"][3] - error_copy["hic_loci"][2]) / 2) - 1
-
-                error_copy["hic_loci"][2] = last_copy["hic_loci"][3] + 1
-
-                ans.append(last_copy)
-                ans.append(error_copy)
-                remove_list.append((error, ans[-1]))
-                # raise NotImplementedError("Not same error but have overlap, wait to solve")
-        else:  # nought overlap
-            ans.append(error)
-
-    print("Filter all error category Done")
-    return ans
+def inference_detector(model, img):
+    pass
 
 
 def main():
-    with open("/home/jzj/Jupyter-Docker/error-detection/swin/ex.pkl", 'rb') as file:
-        error_object = pickle.loads(file.read())
-
-    de_result = de_diff_overlap(error_object)
-    print(de_result)
+    pass
 
 
 if __name__ == "__main__":
